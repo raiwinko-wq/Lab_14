66ae36cbb6ef92f9a24c24fd62daa342324a0d53
Raiwin <raiwin.ko@gmail.com>
2026-01-13 18:12:42 +0700

    lab14_1_fixed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,19 +1,16 @@
-#include <iostream>
-using namespace std;
+template <typename T> 
+void insertionSort(T d[], int N) {
+    for (int i = 1; i < N; i++) {
+        int j = i;
+        
+        while (j > 0 && d[j] > d[j - 1]) {
+            swap(d[j], d[j - 1]);
+            j--;
+        }
 
-template <typename T>
-void insertionSort(T d[],int N){
-
-}
-
-int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
-	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
-	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+        
+        cout << "Pass " << i << ":";
+        for (int k = 0; k < N; k++) cout << d[k] << " ";
+        cout << endl;
+    }
 }
