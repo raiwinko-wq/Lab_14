8e61b0504c210e46d5e03a4875bbe24dd0241b4d
Raiwin <raiwin.ko@gmail.com>
2026-01-13 18:13:24 +0700

    Lab14_2_Fixed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,40 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+void inputMatrix(double mat[][N]) {
+    for (int i = 0; i < N; i++) {
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < N; j++) {
+            cin >> mat[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double A[][N], bool B[][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            // เช็คขอบ
+            if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                B[i][j] = false;
+            } else {
+                // เช็คค่าปัจจุบันเทียบกับ บน ล่าง ซ้าย ขวา
+                if (A[i][j] >= A[i - 1][j] && A[i][j] >= A[i + 1][j] &&
+                    A[i][j] >= A[i][j - 1] && A[i][j] >= A[i][j + 1]) {
+                    B[i][j] = true;
+                } else {
+                    B[i][j] = false;
+                }
+            }
+        }
+    }
+}
+
+void showMatrix(const bool mat[][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << mat[i][j];
+            if (j < N - 1) cout << " ";
+        }
+        cout << "\n";
+    }
+} 
